6adee318818d9950086c1972bbbfac25c174a294
metalicarus <silva.matheusms.mateus83@gmail.com>
2023-01-21 00:27:21 -0300

    unauthorized route and page

---
+++ frontend/tramonto-security/src/pages/Unauthorized.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <h1>Unauthorized</h1>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'UnauthorizedPage',
+};
+</script>
+
+<style scoped>
+
+</style>
